1ad9b0941ea81c76697b8d982aefdb358f2f93cb
Aditi Sharma <sharmaaditi.2212@gmail.com>
2026-09-11 21:52:57 +0530

    Use secret API key for TMDB requests

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import requests
 
 def fetch_poster(movie_id):
-    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={st.secrets['TMDB_API_KEY']}&language=en-US"
     data = requests.get(url).json()
     return "https://image.tmdb.org/t/p/w500/" + data["poster_path"]
 
